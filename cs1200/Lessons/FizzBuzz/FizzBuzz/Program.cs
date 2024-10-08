8dae51f166c5a106927bd57e704e5090caccf450
ishsta <ishstaglass@yahoo.com>
2024-10-08 11:10:18 -0500

    I have lost track where I am

---
+++ cs1200/Lessons/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = 17;
+
+      if (num % 3 == 0 && num % 5 == 0) {
+        Console.WriteLine("FizzBuzz");
+      }else if (num % 3 == 0) {
+        Console.WriteLine("Fizz");
+      } else if (num % 5 == 0) {
+        Console.WriteLine("Buzz");
+      } else {
+        Console.WriteLine(num);
+      }
+    }
+  }
