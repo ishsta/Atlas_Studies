238e072d82c98f61b4e0f63494305cba8e1ac0d7
ishsta <ishstaglass@yahoo.com>
2024-10-10 17:44:03 -0500

    initial AreaCalculator commit

---
+++ cs1200/Exercises/AreaCalculator/AreaCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What type of shape (R)ectangle, (T)riangle, or (C)ircle?");
+            char input = Console.ReadLine().ToUpper().ToCharArray()[0];
+
+            switch (input) {
+                case 'R': {
+                    Console.Write("Enter the length: ");
+                    double length = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the width: ");
+                    double width = Convert.ToDouble(Console.ReadLine());
+                    double area = length * width;
+                    Console.Write($"The area of this rectangle is {area:f2}.");
+                    break;
+                }
+                case 'T': {
+                    Console.Write("Enter the base: ");
+                    double tBase = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the height: ");
+                    double height = Convert.ToDouble(Console.ReadLine());
+                    double area = .5 * (tBase * height);
+                    Console.Write($"The area of this triangle is {area:f2}.");
+                    break;
+                }
+                case 'C': {
+                    Console.Write("Enter the radius: ");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+                    double area = Math.PI * (radius * radius);
+                    Console.Write($"The area of this circle is {area:f2}.");
+                    break;
+                }
+                default:
+                    Console.Write("Invalid shape type!");
+                    break;
+            }
+        }
+    }
+}
