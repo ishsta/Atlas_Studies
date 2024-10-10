f4af7e455ae1aea57791818ef260954a4a2e9e46
ishsta <ishstaglass@yahoo.com>
2024-10-10 17:44:25 -0500

    initial SalesTaxCalculator commit

---
+++ cs1200/Exercises/SalesTaxCalculator/SalesTaxCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SalesTaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the purchase price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the tax rate: ");
+            double tax = Convert.ToDouble(Console.ReadLine());
+
+            double taxTotal = price * (tax / 100);
+            double total = price + taxTotal;
+
+            Console.WriteLine($"\nFor your {price:c} purchase, a {tax}% tax is {taxTotal:c} for a total of {total:c}.");
+        }
+    }
+}
