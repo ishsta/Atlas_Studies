48d2a30efe9603e1cb9a6fd82383112759e6896b
ishsta <ishstaglass@yahoo.com>
2024-10-10 14:15:16 -0500

    initial isVowel commit

---
+++ cs1200/Lessons/isVowel/isVowel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace isVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a letter: ");
+            char input = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (input) {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'Y':
+                    Console.WriteLine("Maybe");
+                    break;
+                default:
+                    Console.WriteLine("Not a Vowel");
+                    break;
+            }
+        }
+    }
+}
