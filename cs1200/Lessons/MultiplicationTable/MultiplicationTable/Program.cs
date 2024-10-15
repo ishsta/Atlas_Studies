776deeb9f492bc735f2808658eb958702988642d
ishsta <ishstaglass@yahoo.com>
2024-10-15 18:32:59 -0500

    initial MultiplicationTable commit

---
+++ cs1200/Lessons/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int startRow;
+
+            Console.Write("Enter starting value for rows: ");
+            int.TryParse(Console.ReadLine(), out startRow);
+
+            Console.Write("Enter ending value for rows: ");
+            int endRow = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter starting value for columns: ");
+            int startCol = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter ending value for columns: ");
+            int endCol = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\t");
+
+            for(int col = startCol; col <= endCol; col++)
+            {
+                Console.Write($"{col}\t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("============================================================");
+
+            for(int row = startRow; row <= endRow; row++)
+            {
+                Console.Write("\t"); // Clean up code, Align middle
+                for(int col = startCol; col <= endCol; col++)
+                {
+                    Console.Write($"{row * col}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
