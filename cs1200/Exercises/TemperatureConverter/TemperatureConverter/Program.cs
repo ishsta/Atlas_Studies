1131bbb3b9481ff57f0ffe6c198b6638d0ec6fc4
ishsta <ishstaglass@yahoo.com>
2024-10-23 23:47:33 -0500

    initial commit

---
+++ cs1200/Exercises/TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace TemperatureConverter;
+
+class Program
+{
+    static void Main()
+    {
+        Temperature t1 = new Temperature();
+
+        Console.Write("Enter temperature (K): ");
+        t1.Kelvin = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The temperature is {t1.Kelvin}K, {t1.Celsius}C, {t1.Fahrenheit}F.");
+
+    }
+}
+
+class Temperature
+{
+    private double _kelvin; // backing field
+
+    public double Kelvin
+    {
+        get { return _kelvin; }
+        set { _kelvin = value; }
+    }
+    public double Celsius
+    {
+        get
+        {
+            return Kelvin - 273.15;
+        }
+    }
+    public double Fahrenheit
+    {
+        get
+        {
+            return Celsius * 9 / 5 + 32;
+        }
+    }
+
+}
